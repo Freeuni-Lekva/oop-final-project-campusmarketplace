0077467c9557e4fe00dffe778ef3f5e971eb0c89
nikasagina <nsagi21@freeuni.edu.ge>
2023-07-13 17:20:43 +0400

    typo

--- src/main/java/marketplace/servlets/LoginServlet.java
+++ src/main/java/marketplace/servlets/LoginServlet.java
@@ -4,11 +4,13 @@ import marketplace.objects.User;
 import marketplace.utils.LoginValidator;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "register", value = "/register")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
