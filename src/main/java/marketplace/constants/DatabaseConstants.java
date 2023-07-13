ce04410093e57a234f541647e596a72db0268c4a
nikasagina <nsagi21@freeuni.edu.ge>
2023-07-13 16:07:22 +0400

    Add listeners & database setup

---
+++ src/main/java/marketplace/constants/DatabaseConstants.java
@@ -0,0 +1,8 @@
+package marketplace.constants;
+
+public class DatabaseConstants {
+    public static final String DATABASE_NAME = "";
+    public static final String DATABASE_USERNAME = "";
+    public static final String DATABASE_PASSWORD = "";
+
+}
---
+++ src/main/java/marketplace/listeners/SessionListener.java
@@ -0,0 +1,16 @@
+package marketplace.listeners;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class SessionListener implements HttpSessionListener {
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+
+    }
+}
