c47eddb3e3dd0641584dc0aa49cb1ef173943319
nikasagina <nsagi21@freeuni.edu.ge>
2023-08-10 01:15:39 +0400

    Add Update request for creating new chats between users

--- src/main/java/marketplace/servlets/ChatServlet.java
+++ src/main/java/marketplace/servlets/ChatServlet.java
@@ -2,6 +2,7 @@ package marketplace.servlets;
 
 import marketplace.annotation.Secure;
 import marketplace.dao.ChatDAO;
+import marketplace.dao.UserDAO;
 import marketplace.objects.User;
 
 import java.io.IOException;
@@ -26,6 +27,20 @@ public class ChatServlet extends HttpServlet {
         List<User> lastMessagedUsers = chatDAO.lastMessages(user.getProfileId(), LAST_N_MESSAGED_USER);
 
         request.setAttribute("user", user);
+        request.setAttribute("lastMessagedUsers", lastMessagedUsers);
+        request.getRequestDispatcher("jsp/chat.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ChatDAO chatDAO = (ChatDAO) request.getServletContext().getAttribute("chatDAO");
+        UserDAO userDAO = (UserDAO) request.getServletContext().getAttribute("userDAO");
+        User user = (User) request.getSession().getAttribute("user");
+        User other = userDAO.getUser(Integer.parseInt(request.getParameter("otherUserId")));
+        List<User> lastMessagedUsers = chatDAO.lastMessages(user.getProfileId(), LAST_N_MESSAGED_USER);
+
+        request.setAttribute("user", user);
+        request.setAttribute("other", other);
         request.setAttribute("lastMessagedUsers", lastMessagedUsers);
         request.getRequestDispatcher("jsp/chat.jsp").forward(request, response);
     }
